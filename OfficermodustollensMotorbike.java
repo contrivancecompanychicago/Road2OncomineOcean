8f03f725171d694055aa01a10da8c514b5cc60da
Eric Hilary Smith <erichilarysmithsr@gmail.com>
2023-03-31 14:23:26 -0500

    Create OfficermodustollensMotorbike.java

---
+++ OfficermodustollensMotorbike.java
@@ -0,0 +1,32 @@
+/**
+ * Motorbike class of the Java program.
+ */
+
+public class OfficermodustollensMotorbike extends Oncomineworld {
+ 
+ 	/**
+     * Creates a new motorbike
+     *
+     * @param c  the motorbike color
+     * @param l  the state of the motorbike lights
+     */
+	public Motorbike(String c, boolean l){
+	    super(c,l);
+	}
+ 	
+ 	
+ 	/**
+     * Returns a String representation of the motorbike
+     *
+     * @return the String representation of the motorbike
+     */
+	public String toString(){
+	   String state = "";
+	    if(lights){
+	        state = "on";
+	    }else{
+	        state = "off";
+	    }
+	    String result = "This motorbike is "+color+", lights are "+state+".";
+	    return result;
+	}
