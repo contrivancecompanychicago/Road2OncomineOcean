bf5e4689ec5492feb6899987ebd8845359c22c7b
Eric Hilary Smith <erichilarysmithsr@gmail.com>
2023-04-10 11:57:40 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,4 +1,7 @@
 package com.road2oncomineocean;
 
-public class Main {
+ class road2oncomineoceanMain {
+   public static void main(String[] args) {
+     System.out.println("This is Road 2 Oncomine Ocean!");
+   }
 }
